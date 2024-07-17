7a39b2e622821026c5399fa274d3f05c5ec0c64f
breno-okra <brenookra@gmail.com>
2024-07-17 13:56:59 -0300

    add components,header,summary and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 
 export function App() {
 
 
   return (
-   <ThemeProvider theme={defaultTheme}>
-    <h1>breno</h1>
-   </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
   )
 }
 
