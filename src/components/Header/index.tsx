d72d35da8385df24355ba893d87fdd8783f3334e
breno-okra <brenookra@gmail.com>
2024-07-18 15:51:23 -0300

    using Radix for create modal access modals

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logoImage from "../../assets/logo.svg"
-export function Header(){
-    return(
+import { NewTransationModal } from '../NewTrabsactionModal';
+export function Header() {
+    return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImage} alt="" />
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <NewTransationModal />
+                </Dialog.Root >
             </HeaderContent>
         </HeaderContainer>
     )
