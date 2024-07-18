9cbadadac031fa9d3d349215de9d8750c0639196
breno-okra <brenookra@gmail.com>
2024-07-18 15:53:23 -0300

    learn to use inset:0;

--- src/components/NewTrabsactionModal/style.ts
+++ src/components/NewTrabsactionModal/style.ts
@@ -4,7 +4,13 @@ export const Overlay = styled(Dialog.Overlay)`
     position: fixed;
     width: 100vw;
     height: 100vh;
-    
+    inset: 0;
+  /*   
+    inset:0; é o mesmo que:
+    top:0;
+    right: 0;
+    left: 0;
+    bottom: 0; */
 
 `
 export const Content = styled(Dialog.Content)`
