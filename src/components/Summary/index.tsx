8f90fc53a0e76c7682d64a8007f64af7632afe3e
breno-okra <brenookra@gmail.com>
2024-07-19 16:36:44 -0300

    use reduce for calc values of the transactions, income,outcome,total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,7 +5,24 @@ import { TransactionsContext } from "../../contexts/transactionsContext";
 
 export function Summary() {
     const {transactions} = useContext(TransactionsContext)
-    
+    // primeiro a função, depois a estrutura de dados inicial
+    // const summary = transactions.reduce(() => {},{income:0,outcome:0,total:0})
+
+    //acc - acumulate que é os dados iniciais {income:0,outcome:0,total:0}
+    const summary = transactions.reduce(
+        (acc,transactions) => {
+            if(transactions.type == 'income'){
+                acc.income += transactions.price
+                acc.total += transactions.price
+            }else{
+                acc.outcome += transactions.price
+                acc.total -= transactions.price
+            }
+            return acc
+
+        },
+        {income:0,outcome:0,total:0}
+    )
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -13,21 +30,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.income}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.outcome}</strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>R$ {summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
