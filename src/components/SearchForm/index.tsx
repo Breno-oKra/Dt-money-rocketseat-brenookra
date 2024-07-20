c0734ffaab52133c870924edf412c13c691cefb1
breno-okra <brenookra@gmail.com>
2024-07-20 19:13:04 -0300

    finish project of course

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./style";
 import { useForm } from "react-hook-form";
@@ -7,20 +7,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../contexts/transactionsContext";
 import { useContextSelector } from "use-context-selector";
 
-
-
-/* 
-    por que um componente renderiza?
-     -  hooks changed
-     -  props changed 
-     -  parent rerendered  
-*/
-
 const searchFormSchema = z.object({
     query:z.string()
 })
 type SearchFormInput = z.infer<typeof searchFormSchema>
-function SearchFormComponent(){
+export function SearchForm(){
     /* const {fetchTransactions} = useContext(TransactionsContext) */
     const fetchTransactions = useContextSelector(TransactionsContext,(context) =>{
         return context.fetchTransactions
@@ -36,7 +27,7 @@ function SearchFormComponent(){
         <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
             <input 
             type="text" 
-            placeholder="Busque uma tranza ação"
+            placeholder="Busque uma transação"
             {...register("query")} />
             <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
@@ -45,11 +36,3 @@ function SearchFormComponent(){
         </SearchFormContainer>
     )
 }
-/**
- * o memo compara tudo, se os hooks mudaram,props em relação a versão anterior a nova que veio
- * se mudou algo,ele vai permitir a nova renderização
- * o memo foi usado nesse caso somente para exemplo
- * ele deve ser usado somente em componentes complexos e com muitos calculos, para que esses componentes não seja renderizados desnecessariamente
- * usar o memo em casos simples como este pode ocasionar ate uma perca na perfomace
- */
-export const SearchForm = memo(SearchFormComponent)
